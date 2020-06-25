36fd02feb598097ce87d975e0180982834445355
Kayla Melton <kannmelton2@gmail.com>
2020-06-24 20:58:16 -0500

    wip

--- src/components/pages/MySightings/MySightings.js
+++ src/components/pages/MySightings/MySightings.js
@@ -1,12 +1,41 @@
 import React from 'react';
 
+import authData from '../../../helpers/data/authData';
+import smash from '../../../helpers/data/smash';
+
+import Sightings from '../../shared/Sightings/Sightings';
+
 import './MySightings.scss';
 
 class MySightings extends React.Component {
+  state = {
+    sightings: [],
+  }
+
+  getSigtingsInfo = () => {
+    const currentUid = authData.getUid();
+    smash.getSightingByUidLocation(currentUid)
+      .then((sightings) => {
+        this.setState({ sightings });
+      })
+      .catch((err) => console.error('could not get sightings with locations: ', err));
+  }
+
+  componentDidMount() {
+    this.getSigtingsInfo();
+  }
+
   render() {
+    const { sightings } = this.state;
+
+    const buildSightings = sightings.map((sighting) => (
+      <Sightings key={sighting.id} sighting={sighting} />
+    ));
+
     return (
       <div className="MySightings">
         <h1>My Sightings</h1>
+        {buildSightings}
       </div>
     );
   }
--- src/helpers/data/smash.js
+++ src/helpers/data/smash.js
@@ -1,5 +1,6 @@
 import locationData from './locationData';
 import sightingsData from './sightingsData';
+import butterflyData from './butterflyData';
 
 const getSightingLocations = (butterflyId) => new Promise((resolve, reject) => {
   sightingsData.getSightingsByButterflyId(butterflyId)
@@ -19,4 +20,45 @@ const getSightingLocations = (butterflyId) => new Promise((resolve, reject) => {
     .catch((err) => reject(err));
 });
 
-export default { getSightingLocations };
+const getSightingByUidLocation = (uid) => new Promise((resolve, reject) => {
+  sightingsData.getSightingsByUid(uid)
+    .then((sightingsResponse) => {
+      locationData.getLocations().then((locationsResponse) => {
+        const finalSightings = [];
+        sightingsResponse.forEach((singleSighting) => {
+          const sighting = { ...singleSighting };
+          const selectedLocation = locationsResponse.find((x) => x.id === sighting.locationId);
+          sighting.city = selectedLocation.city;
+          sighting.state = selectedLocation.state;
+          finalSightings.push(sighting);
+        });
+        console.error('sighting: ', finalSightings);
+        resolve(finalSightings);
+      });
+    })
+    .catch((err) => reject(err));
+});
+
+const gettingFinalSightings = (butterflyId) => new Promise((resolve, reject) => {
+  sightingsData.getSightingsByButterflyId(butterflyId)
+    .then((sightingsResponse) => {
+      locationData.getLocations().then((locationsResponse) => {
+        butterflyData.getButterflies().then((butterflyResponse) => {
+          const finalSightings = [];
+          sightingsResponse.forEach((singleSighting) => {
+            const sighting = { ...singleSighting };
+            const selectedLocation = locationsResponse.find((x) => x.id === sighting.locationId);
+            sighting.city = selectedLocation.city;
+            sighting.state = selectedLocation.state;
+            const selectedButterfly = butterflyResponse.find((x) => x.id === sighting.butterflyId);
+            sighting.butterflyCommonName = selectedButterfly.commonName;
+            finalSightings.push(sighting);
+          });
+          resolve(finalSightings);
+        });
+      });
+    })
+    .catch((err) => reject(err));
+});
+
+export default { getSightingLocations, getSightingByUidLocation, gettingFinalSightings };
