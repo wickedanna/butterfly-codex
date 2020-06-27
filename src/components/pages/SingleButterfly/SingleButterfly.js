42af84dfacf745aa744b3adb66a63afddee74858
Kayla Melton <kannmelton2@gmail.com>
2020-06-27 11:15:32 -0500

    added styling

--- src/components/pages/SingleButterfly/SingleButterfly.js
+++ src/components/pages/SingleButterfly/SingleButterfly.js
@@ -57,20 +57,20 @@ class SingleButterfly extends React.Component {
           <img src={butterfly.mainImage} className="main-image" alt="please refer to description below"/>
         </div>
         <h2 className="mx-3 single-butterfly-title">The {butterfly.commonName} Butterfly</h2>
-        <h4 className="mx-3 basic-info">Common Name: {butterfly.commonName}</h4>
-        <h4 className="mx-3 basic-info">Scientific Name: {butterfly.scientificName}</h4>
-        <h4 className="mx-3 basic-info">Type: {butterfly.type}</h4>
-        <h4 className="mx-3 basic-info">Physical Characteristics</h4>
-        <h5 className="mx-3 basic-info">Adult</h5>
-        <p className="mx-3 basic-info">{butterfly.adultAppearance}</p>
-        <h5 className="mx-3 basic-info">Egg, Larvae, Pupae</h5>
-        <p className="mx-3 basic-info">{butterfly.larvaeAppearance}</p>
-        <h4 className="mx-3 basic-info">What do they Eat?</h4>
-        <p className="mx-3 basic-info">{butterfly.diet}</p>
-        <h4 className="mx-3 basic-info">What kind of area do these butterflies prefer?</h4>
-        <p className="mx-3 basic-info">{butterfly.preferredClimate} {butterfly.commonlyFound}</p>
-        <h4 className="mx-3 basic-info">Lifespan</h4>
-        <p className="mx-3 basic-info">{butterfly.lifespan} {butterfly.activeWhen}</p>
+        <h4 className="mx-3 basic-info-title">Common Name: <span className="basic-info-text">{butterfly.commonName}</span></h4>
+        <h4 className="mx-3 basic-info-title">Scientific Name: <span className="basic-info-text">{butterfly.scientificName}</span></h4>
+        <h4 className="mx-3 basic-info-title">Type: <span className="basic-info-text">{butterfly.type}</span></h4>
+        <h4 className="mx-3 basic-info-title">Physical Characteristics</h4>
+        <h5 className="mx-3 basic-info-subtitle">Adult</h5>
+        <p className="mx-3 basic-info-text">{butterfly.adultAppearance}</p>
+        <h5 className="mx-3 basic-info-subtitle">Egg, Larvae, Pupae</h5>
+        <p className="mx-3 basic-info-text">{butterfly.larvaeAppearance}</p>
+        <h4 className="mx-3 basic-info-title">What do they Eat?</h4>
+        <p className="mx-3 basic-info-text">{butterfly.diet}</p>
+        <h4 className="mx-3 basic-info-title">What kind of area do these butterflies prefer?</h4>
+        <p className="mx-3 basic-info-text">{butterfly.preferredClimate} {butterfly.commonlyFound}</p>
+        <h4 className="mx-3 basic-info-title">Lifespan</h4>
+        <p className="mx-3 basic-info-text">{butterfly.lifespan} {butterfly.activeWhen}</p>
         </div>
         <div className="butterfly-sightings container d-flex flex-wrap">
           <h3 className="mx-3 single-butterfly-title">Sightings:</h3>
