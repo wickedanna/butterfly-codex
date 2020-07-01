46a2aece1db3ae240f00f1704236b94e9cb915e7
Kayla Melton <kannmelton2@gmail.com>
2020-07-01 12:52:13 -0500

    states render in alphabetical order

--- src/helpers/data/locationData.js
+++ src/helpers/data/locationData.js
@@ -13,6 +13,12 @@ const getLocations = () => new Promise((resolve, reject) => {
           allLocations[locationId].id = locationId;
           locations.push(allLocations[locationId]);
         });
+        // eslint-disable-next-line no-confusing-arrow
+        locations.sort((a, b) => (a.state > b.state) ? 1 : -1);
+        // the line above uses the sort method on the locations array
+        // it takes a & b, which represent the indices of two objects within the array and compares them
+        // it gives a value of 1 or -1 to determine where they should go, and ends with the objects
+        // in alphabetical order by the state property
       }
       resolve(locations);
     })
